6c579a8b2309666561edcbc650e12c29a078de9e
jasminejeane <jasminejeane@gmail.com>
2018-05-11 08:27:24 -0700

    replaced promise

--- imperial-starfleet/src/app/info-window/info-window.component.ts
+++ imperial-starfleet/src/app/info-window/info-window.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import { ActivatedRoute }   from '@angular/router';
+import { Subject } from 'rxjs/Subject';
+
 
 @Component({
   selector: 'app-info-window',
@@ -11,22 +13,24 @@ import { ActivatedRoute }   from '@angular/router';
 export class InfoWindowComponent implements OnInit {
 
 	dataBanks;
+  searchSubject = new Subject(); //add this property
+
 
   constructor(
   	private http: Http,
   	private route: ActivatedRoute
   ) { }
 
+   findTurret(turretNumber){
+    this.searchSubject.next(turretNumber); // add this line
+	}
+
   ngOnInit() {
-  	this.route.params.forEach( param => {
-  		this.findTurret(param.id);
-  	});
+  	this.searchSubject.subscribe( param => {
+    this.http.get('http://localhost:3000/api/turret/' + param.id)
+    .subscribe(response => this.dataBanks = response.json());  	
+  });
   }
 
-  findTurret(turretNumber){
-    this.http.get('http://localhost:3000/api/turret/'+turretNumber)
-    .toPromise()
-    .then(response => this.dataBanks = response.json());
-	}
 
 }
