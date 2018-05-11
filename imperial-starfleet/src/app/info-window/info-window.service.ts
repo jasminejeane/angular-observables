d23b25146f47fcc33c7b0ec98bf11df1fe36b114
jasminejeane <jasminejeane@gmail.com>
2018-05-11 08:48:27 -0700

    services

---
+++ imperial-starfleet/src/app/info-window/info-window.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { Http } from '@angular/http';
+
+
+@Injectable()
+export class SearchService{
+
+constructor(private http: Http){}
+
+createAPIObservable(turretNumber){
+return this.http.get('http://localhost:3000/api/turret/' + turretNumber)
+}
+
+}
